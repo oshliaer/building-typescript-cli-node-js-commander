5c58825e525c8f90e6eaef2b891689dffc25b7c7
Alexander Ivanov <oshli.a.er@gmail.com>
2025-01-04 20:14:12 +0300

    feat: add options
    Signed-off-by: Alexander Ivanov <oshli.a.er@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,13 @@ import figlet from 'figlet';
 const program = new Command();
 
 console.log(figlet.textSync('Dir Manager'));
+
+program
+  .version('1.0.0')
+  .description('An example CLI for managing a directory')
+  .option('-l, --ls  [value]', 'List directory contents')
+  .option('-m, --mkdir <value>', 'Create a directory')
+  .option('-t, --touch <value>', 'Create a file')
+  .parse(process.argv);
+
+const options = program.opts();
